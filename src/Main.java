81068d0b98ac5fffa056d3dfb51a1945efcd7f27
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-10-12 00:46:30 +0900

    lib

--- src/Main.java
+++ src/Main.java
@@ -147,6 +147,26 @@ class lib{
         }
         return sp == s.length();
     }
+    static boolean isPalindrome(String s){
+        StringBuilder sb = new StringBuilder(s);
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        if(sb.toString().equals(sb.reverse().toString())){
+            return true;
+        }
+        return false;
+    }
+    static boolean isPalindrome(int n){
+        String s = Integer.toString(n);
+        return isPalindrome(s);
+    }
+    static boolean isPalindrome(long n){
+        String s = Long.toString(n);
+        return isPalindrome(s);
+    }
 }
 
 
